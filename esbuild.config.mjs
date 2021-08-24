a797adad3d52eff07db70e4e28bd37fc2d6e1c08
Taniguchi Masaki <window100@gmail.com>
2021-08-24 19:39:32 +0900

    Update build configurations

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,49 +1,44 @@
 import { build } from "esbuild";
-import { stat } from "fs";
-import { copy } from "fs-extra";
+import { copy, emptyDir } from "fs-extra";
 import serve, { error as logError, log } from "create-serve";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 const watchChanges = process.argv.slice(2).includes("--watch");
 const distRoot = "dist";
 
-[
-  ["src/index.html", "dist/index.html"],
-].map(async ([src, dest]) => {
-  let mtimeSrc = 0;
-  let mtimeDest = 0;
-  // noinspection JSVoidFunctionReturnValueUsed
-  await Promise.all([
-    stat(src, (err, stats) => mtimeSrc = stats?.mtime),
-    stat(dest, (err, stats) => mtimeDest = stats?.mtime)
-  ]);
-  await copy(src, dest, {
-    overwrite: (mtimeSrc - mtimeDest > 0),
-    preserveTimestamps: true
+(async () => {
+  await emptyDir("dist");
+
+  // copy assets
+  [
+    ["src/index.html", "dist/index.html"],
+  ].map(async ([src, dest]) => {
+    await copy(src, dest);
   });
-});
 
-build({
-  entryPoints: ["src/index.ts"],
-  outdir: distRoot,
-  bundle: true,
-  charset: "utf8",
-  minify: !isDevelopment,
-  platform: "browser",
-  sourcemap: isDevelopment,
-  target: ["es2020"],
-  tsconfig: "tsconfig.json",
-  write: true,
-  watch: watchChanges && {
-    onRebuild(error) {
-      serve.update();
-      error ? logError("× Failed") : log(`[${new Date().toLocaleString()}] ✓ Updated`);
+  // options for esbuild
+  build({
+    entryPoints: ["src/index.tsx"],
+    outdir: distRoot,
+    bundle: true,
+    charset: "utf8",
+    minify: !isDevelopment,
+    platform: "browser",
+    sourcemap: isDevelopment,
+    target: ["es2020"],
+    tsconfig: "tsconfig.json",
+    write: true,
+    watch: watchChanges && {
+      onRebuild(error) {
+        serve.update();
+        error ? logError("× Failed") : log(`[${new Date().toLocaleString()}] ✓ Updated`);
+      },
     },
-  },
-}).then(_ => {});
+  }).then(_ => {});
 
-// options for Serve
-watchChanges && serve.start({
-  port: 7000,
-  root: distRoot,
-});
+  // options for 'Serve'
+  watchChanges && serve.start({
+    port: 7000,
+    root: distRoot,
+  });
+})();
